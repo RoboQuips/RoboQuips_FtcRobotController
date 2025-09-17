a8aec81c46df382d3a08a321c77f1e068daa45b4
RoboQuips <roboquips@gmail.com>
2025-09-16 21:35:01 -0400

    HO - UpdateController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -10,11 +10,18 @@ public class HelloWorld extends OpMode {
 
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        int teamNumber = 30457;
+
+        telemetry.addData("Team Number", teamNumber);
     }
 
     @Override
     public void loop() {
+        double speedForward = -gamepad1.left_stick_y;
+
+        telemetry.addData("x", gamepad1.left_stick_x);
+        telemetry.addData("y", speedForward);
+        telemetry.addData("a button", gamepad1.a);
 
     }
 }
